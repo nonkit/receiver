d45054b797860cd7a6e55127e81531c89989a05f
Nonki Takahashi <nonkit@outlook.jp>
2021-05-20 20:15:57 +0900

    changed to pass read command ":"

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
-radio.onReceivedString(function (receivedString) {
-    serial.writeLine(receivedString)
-    basic.showString(receivedString)
+input.onButtonPressed(Button.A, function () {
+    basic.showString(line)
 })
+serial.onDataReceived(serial.delimiters(Delimiters.Colon), function () {
+    radio.sendString(":")
+})
+radio.onReceivedString(function (receivedString) {
+    line = receivedString
+    serial.writeLine(line)
+})
+let line = ""
 radio.setGroup(1)
+line = "N/A"
